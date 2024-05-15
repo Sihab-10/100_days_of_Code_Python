02d9033b2184987999c57b4baadc7f8c39838ea1
Sihab Sarar Chistee <60611053+Sihab-10@users.noreply.github.com>
2024-05-15 10:17:29 +0600

    break and continue added

---
+++ day_19/main.py
@@ -0,0 +1,34 @@
+'''
+======================================= break statement ==========================================================
+'''
+
+'''
+The break statement enables a program to skip over a part of the code. A break statement terminates the very loop it lies within. 
+'''
+# for i in range(1,100,1):
+#     print(i, end=" ")
+#     if(i==50):
+#         break
+#     else:
+#         print("Mississippi")
+# print("Thank you")
+
+# for i in range(15):
+#     if(i == 10 ):
+#         break
+#     print("5 X", i+1, "=", 5 * (i+1))
+# print("breaking out of  the loop")
+
+
+'''
+============================================ Continue Statement ==========================================
+'''
+# Continue Statement
+
+# The continue statement skips the rest of the loop statements and causes the next iteration to occur. 
+
+# for i in range(15):
+#     if(i == 10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X", i, "=", 5 * i)
